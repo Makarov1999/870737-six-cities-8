77d4ee2ab706ab42239f60df58282cb4877aa50e
Makarov1999 <makarov.e1999@yandex.ru>
2021-11-14 21:19:07 +0500

    Add property page2

--- project/src/services/offer/offer.types.ts
+++ project/src/services/offer/offer.types.ts
@@ -1,5 +1,4 @@
 import TCityPlaceCard from '../../types/city-place-card';
-import TCityPlaceCardApi from '../../types/city-place-card-api';
 
 export type TSuccessOfferFunction = (data: TCityPlaceCard) => void;
 export type TSuccessOfferNeabyFunction = (data: TCityPlaceCard[]) => void;
